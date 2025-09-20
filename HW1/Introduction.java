e53d55741c7d94e24e6c5c1db6186a41a5e91d9c
Nguyen Tien Duong <24021442@vnu.edu.vn>
2025-09-20 15:12:06 +0700

    Update Introduction.java

--- HW1/Introduction.java
+++ HW1/Introduction.java
@@ -2,7 +2,7 @@ import java.sql.SQLOutput;
 
 public class Introduction {
     public static void main(String agrs[]) {
-        System.out.println("Nguyễn Tiến Dương\t24021442\tINT2204_11\tnguyentienduong2006\t24021442@vnu.edu.vn");
+        System.out.println("Nguyễn Tiến Dương\t24021442\tK69I-CS5\tnguyentienduong2006\t24021442@vnu.edu.vn");
         System.out.println("9 bottles of beer on the wall, 9 bottles of beer.");
         System.out.println("Take one down, pass it around,");
         System.out.println("8 bottles of beer on the wall, 8 bottles of beer.");
